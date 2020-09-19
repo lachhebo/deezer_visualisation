a89c4ae334fa183e019fcb8d2f33578708bff2f3
Ismaël <ismael.lachheb@protonmail.com>
2020-09-19 14:25:20 +0200

    feat: add feature engineering enrich step

--- main.py
+++ main.py
@@ -1,62 +1,38 @@
-import requests
+from datetime import datetime
+
 import pandas as pd
 import typer
-from datetime import datetime
+
+from api_manager import get_music_history, get_music_favorite
+from data_processing import process_track_history, process_favorite_tracks
 
 now = datetime.now()
 app = typer.Typer()
 
 
-def get_music_history(access_token, user_id):
-    params_token = (
-        ('access_token', access_token),
-    )
-
-    url = f'https://api.deezer.com/user/{user_id}/history&limit=1000'
-    response = requests.get(url, params=params_token).json()
-    result = response.get('data')
-
-    url = response.get('next')
-    response = requests.get(url, params=params_token).json().get('data')
-    result = result + response
-    return result
+@app.command()
+def process_history(file_path: str, folder_to_save: str):
+    process_track_history(file_path, folder_to_save)
 
 
-def get_music_favorite(access_token, user_id, limit=2000):
-    params = (
-        ('access_token', access_token),
-    )
-
-    url = f'https://api.deezer.com/user/{user_id}/playlists'
-    playlist_list = requests.get(url, params=params).json().get('data')
-    id_playlist = _find_loved_tracks_playlist(playlist_list)
-
-    url_playlist = f'https://api.deezer.com/playlist/{id_playlist}&limit={limit}'
-    response = requests.get(url_playlist, params=params).json().get('tracks').get('data')
-
-    return response
-
-
-def _find_loved_tracks_playlist(playlist_list):
-    for playlist in playlist_list:
-        if playlist['is_loved_track']:
-            id_playlist = playlist['id']
-    return id_playlist
+@app.command()
+def process_favorites(file_path: str, folder_to_save: str):
+    process_favorite_tracks(file_path, folder_to_save)
 
 
 @app.command()
 def history(access_token: str, user_id: str, filename: str = 'listening_history'):
-    history = get_music_history(access_token, user_id)
-    history = pd.DataFrame(history).set_index('id')
-    history.to_csv(filename + '_' + now.strftime('%Y-%m-%d') + '.csv')
+    music_history = get_music_history(access_token, user_id)
+    df_history = pd.DataFrame(music_history).set_index('id')
+    df_history.to_csv(filename + '_' + now.strftime('%Y-%m-%d-%H') + '.csv')
     typer.echo(f'listening history saved in {filename}')
 
 
 @app.command()
 def favorite(access_token: str, user_id: str, filename: str = 'favorite_tracks'):
-    favorite = get_music_favorite(access_token, user_id)
-    favorite = pd.DataFrame(favorite).set_index('id')
-    favorite.to_csv(filename + '_' + now.strftime('%Y-%m-%d') + '.csv')
+    favorite_musics = get_music_favorite(access_token, user_id)
+    df_favorite = pd.DataFrame(favorite_musics).set_index('id')
+    df_favorite.to_csv(filename + '_' + now.strftime('%Y-%m-%d-%H') + '.csv')
     typer.echo(f'favorite tracks saved in {filename}')
 
 
