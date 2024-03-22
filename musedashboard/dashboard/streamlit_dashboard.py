17986ab70dc2d28a4e0623d0ee64471b6d550376
Ismaïl Lachheb <ismael.lachheb@protonmail.com>
2024-03-22 09:47:56 +0100

    Update streamlit_dashboard.py

--- musedashboard/dashboard/streamlit_dashboard.py
+++ musedashboard/dashboard/streamlit_dashboard.py
@@ -17,8 +17,8 @@ def get_history():
 DF_HISTORY = get_history()
 
 def apply_filter(df_history):
-    # return df_history
-    return df_history[df_history.datetime > dt.datetime.now() - dt.timedelta(days=30)]
+    return df_history
+    #return df_history[df_history.datetime > dt.datetime.now() - dt.timedelta(days=30)]
 
 st.header("Top artists")
 fig = px.bar(
